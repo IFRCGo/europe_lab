3f96c9884201d84f43c41ca739e80a2739d846ae
ryales <rcyales@gmail.com>
2017-08-07 16:53:49 +0200

    areas of focus section

--- js/site.js
+++ js/site.js
@@ -26,6 +26,7 @@ function hxlProxyToJSON(input,headers){
     return output;
 }
 
+// Number formatting
 function niceFormatNumber(num,round){
     if(isNaN(parseFloat(num))){
         return num;
@@ -50,6 +51,7 @@ function niceFormatNumber(num,round){
     }
 }
 
+// Generate Areas of Focus Figures
 function loadKeyFigures(url){
     var hxlurl = 'https://proxy.hxlstandard.org/data.json?strip-headers=on&url='+url;
     $.ajax({
@@ -58,20 +60,26 @@ function loadKeyFigures(url){
             dataType: 'json',
             success: function(result){
                 var data = hxlProxyToJSON(result);
-                var html = '<div class="column small-up-2 medium-up-3 large-up-4"><h2>Key Figures</h2>';
+                console.log(data);
+                var html = '<div class="column small-up-2 medium-up-3 large-up-4"><h1>Areas of Focus: People Reached</h1>';
                 data.forEach(function(d){
-                    html+='<div class="column"><div class="card no-border"><h4 class="keyfiguretitle text-center minheight">'+d['#meta+title']+'</h4><p class="keyfigure text-center">'+niceFormatNumber(d['#indicator'])+'</p><p class="small text-center">Source: <a href="'+d['#meta+url']+'" target="_blank">'+d['#meta+source']+'</a></p></div></div>'
+                    html+='<div class="column"><div class="card padded"><img alt="'+d['#meta+title']+' Area of Focus" title="'+d['#meta+title']+'" class="text-center" width="100" src="img/'+d['#meta+icon']+'.svg"><h5 class="keyfiguretitle text-center">'+d['#meta+title']+'</h5><p class="keyfigure text-center">'+niceFormatNumber(d['#indicator'])+'</p><p class="small text-center">Source: <a href="'+d['#meta+url']+'" target="_blank">'+d['#meta+source']+'</a></p></div></div>'
                 });
-                html+='</div>'; //closing div for KF
-                $('#keyfigures').html(html);
+                html+='</div>'; //closing div
+
+                // Send html to proper id
+                $('#aof').html(html);
             }
     });
 }
 
+// Generate tables for appeals and DREFs
 function createAppealsTable(data){
+    // Initialize html tables
     var html1 = "";
     var html2 = "";
-    console.log(data);
+
+    // Run through data and prep for tables
     data.forEach(function(d,i){
         var url = 'http://ifrcgo.org/appeals/'+d['#meta+id'].toLowerCase()
 
@@ -82,17 +90,19 @@ function createAppealsTable(data){
             html2 += '<tr><td><a href="'+url+'" target="_blank">'+d['#crisis+name']+'</a></td><td>'+d['#date+start']+'</td><td>'+d['#date+end']+'</td><td>'+niceFormatNumber(d['#targeted'])+'</td><td>'+niceFormatNumber(d['#meta+value'])+'</td><td><a href="'+url+'" target="_blank">'+d['#meta+id']+'</a></td></tr>';
         }
     });
-    console.log(html1);
+    // Send data to appeals or DREFs html tables
     $('#appealstable').append(html1);
     $('#drefstable').append(html2);
+    
     data.forEach(function(d,i){
         createPie('#coverage'+i,60,10,d['#meta+coverage'].substring(0, d['#meta+coverage'].length - 1)/100);
     });
 }
 
+// Generation of appeal coverage pie charts
 function createPie(id,width,inner,percent){
 
-    console.log(percent);
+    //console.log(percent);
     var svg = d3.select(id).append("svg")
         .attr("width", width)
         .attr("height", width);
@@ -128,31 +138,26 @@ function createPie(id,width,inner,percent){
         .style("text-anchor", "middle");
 }
 
-var appeals = ['MDRTR003','MDRUA007','MDRIT002','MDRGR001','MDRRS011','MDRTJ025','MDRKG012','MDRRU021','MDRHU006','MDRTJ026'];
-var hxlAppealString = '';
-appeals.forEach(function(appeal,i){
-    hxlAppealString+= '&select-query02-0'+(i+1)+'=%23meta%2Bid%3D'+appeal;
-});
-//console.log(hxlAppealString);
-
-/*var hxlAppealsCallURL = 'https://proxy.hxlstandard.org/data.json?merge-tags03=%23meta%2Bcoverage%2C%23meta%2Bfunding&filter04=replace-map&replace-map-url04=https%3A//docs.google.com/spreadsheets/d/1hTE0U3V8x18homc5KxfA7IIrv1Y9F1oulhJt0Z4z3zo/edit%3Fusp%3Dsharing&merge-keys05=%23country%2Bname&filter03=merge&url=https%3A//docs.google.com/spreadsheets/d/19pBx2NpbgcLFeWoJGdCqECT2kw9O9_WmcZ3O41Sj4hU/edit%23gid%3D0&merge-url05=https%3A//docs.google.com/spreadsheets/d/1GugpfyzridvfezFcDsl6dNlpZDqI8TQJw-Jx52obny8/edit%3Fusp%3Dsharing&merge-keys03=%23meta%2Bid&filter02=select&filter01=clean&strip-headers=on&clean-date-tags01=%23date&force=on&merge-url03=https%3A//docs.google.com/spreadsheets/d/1rVAE8b3uC_XIqU-eapUGLU7orIzYSUmvlPm9tI0bCbU/edit%23gid%3D0&filter05=merge&merge-tags05=%23country%2Bcode'+hxlAppealString;*/
-
+// Identify URL for appeals and DREFs data
 var hxlAppealsCallURL = 'https://proxy.hxlstandard.org/data.json?strip-headers=on&filter01=select&select-query01-01=%23region%2Bname%3DEurope&filter03=merge&clean-date-tags01=%23date&filter02=select&merge-keys03=%23meta%2Bid&filter04=replace-map&filter05=merge&merge-tags03=%23meta%2Bcoverage%2C%23meta%2Bfunding&select-query02-01=%23date%2Bend%3E999999&merge-keys05=%23country%2Bname&merge-tags05=%23country%2Bcode&filter01=clean&replace-map-url04=https%3A//docs.google.com/spreadsheets/d/1hTE0U3V8x18homc5KxfA7IIrv1Y9F1oulhJt0Z4z3zo/edit%3Fusp%3Dsharing&merge-url03=https%3A//docs.google.com/spreadsheets/d/1rVAE8b3uC_XIqU-eapUGLU7orIzYSUmvlPm9tI0bCbU/edit%23gid%3D0&merge-url05=https%3A//docs.google.com/spreadsheets/d/1GugpfyzridvfezFcDsl6dNlpZDqI8TQJw-Jx52obny8/edit%3Fusp%3Dsharing&url=https%3A//docs.google.com/spreadsheets/d/19pBx2NpbgcLFeWoJGdCqECT2kw9O9_WmcZ3O41Sj4hU/edit%23gid%3D0';
-var today = new Date();
-var dd = today.getDate();
-var mm = today.getMonth()+1;
-var yyyy = today.getFullYear();
-if(dd<10) {
-    dd='0'+dd
-}
-if(mm<10) {
-    mm='0'+mm
-}
-var date = yyyy + '-' + mm + '-' + dd;
+
+// Determine today's date
+    var today = new Date();
+    var dd    = today.getDate();
+    var mm    = today.getMonth()+1;
+    var yyyy  = today.getFullYear();
+    if(dd<10) {
+        dd='0'+dd
+    }
+    if(mm<10) {
+        mm='0'+mm
+    }
+    var date = yyyy + '-' + mm + '-' + dd;
+
+// Replace placeholder in URL with today's date to get only active appeals and DREFs
 hxlAppealsCallURL = hxlAppealsCallURL.replace('999999',date);
 
-var hxlDocumentsCallURL = 'https://proxy.hxlstandard.org/data.json?filter01=select&select-query01-01=%23region%2Bname%3DEurope&filter02=select&strip-headers=on&url=https%3A//docs.google.com/spreadsheets/d/1gJ4N_PYBqtwVuJ10d8zXWxQle_i84vDx5dHNBomYWdU/edit%3Fusp%3Dsharing'+hxlAppealString;
-
+// Get appeals and DREFs data
 $.ajax({
     type: 'GET',
     url: hxlAppealsCallURL,
@@ -163,37 +168,29 @@ $.ajax({
     }
 });
 
-/*var appeals = ;
+/* 
+// Manual defining of appeals if needed for documents table creation
+// NOTE: too many appeals so the URL is too long and cuts off even if you wanted this to work
+var appeals = ['MDRTR003','MDRUA007','MDRIT002','MDRGR001','MDRRS011','MDRTJ025','MDRKG012','MDRRU021','MDRHU006','MDRTJ026'];
+
 var hxlAppealString = '';
+
 appeals.forEach(function(appeal,i){
     hxlAppealString+= '&select-query02-0'+(i+1)+'=%23meta%2Bid%3D'+appeal;
 });
 
-var region = 'Europe';
+// Identify URL for documents data
+var hxlDocumentsCallURL = 'https://proxy.hxlstandard.org/data.json?filter01=select&select-query01-01=%23region%2Bname%3DEurope&filter02=select&strip-headers=on&url=https%3A//docs.google.com/spreadsheets/d/1gJ4N_PYBqtwVuJ10d8zXWxQle_i84vDx5dHNBomYWdU/edit%3Fusp%3Dsharing'+hxlAppealString;
 
-var hxlAppealsCallURL = 'https://proxy.hxlstandard.org/data.json?merge-tags03=%23meta%2Bcoverage%2C%23meta%2Bfunding&filter04=replace-map&replace-map-url04=https%3A//docs.google.com/spreadsheets/d/1hTE0U3V8x18homc5KxfA7IIrv1Y9F1oulhJt0Z4z3zo/edit%3Fusp%3Dsharing&merge-keys05=%23country%2Bname&filter03=merge&url=https%3A//docs.google.com/spreadsheets/d/19pBx2NpbgcLFeWoJGdCqECT2kw9O9_WmcZ3O41Sj4hU/edit%23gid%3D0&merge-url05=https%3A//docs.google.com/spreadsheets/d/1GugpfyzridvfezFcDsl6dNlpZDqI8TQJw-Jx52obny8/edit%3Fusp%3Dsharing&merge-keys03=%23meta%2Bid&filter02=select&filter01=clean&strip-headers=on&clean-date-tags01=%23date&force=on&merge-url03=https%3A//docs.google.com/spreadsheets/d/1rVAE8b3uC_XIqU-eapUGLU7orIzYSUmvlPm9tI0bCbU/edit%23gid%3D0&filter05=merge&merge-tags05=%23country%2Bcode&select-query02-01=%23region%2Bname%3DEurope'+hxlAppealString;
-var hxlDocumentsCallURL = 'https://proxy.hxlstandard.org/data.json?filter02=select&strip-headers=on&url=https%3A//docs.google.com/spreadsheets/d/1gJ4N_PYBqtwVuJ10d8zXWxQle_i84vDx5dHNBomYWdU/edit%3Fusp%3Dsharing'+hxlAppealString;
-
-function onlyUnique(value, index, self) { 
-    return self.indexOf(value) === index;
-}
-
-$.ajax({
-    type: 'GET',
-    url: hxlAppealsCallURL,
-    dataType: 'json',
-    success:function(response){
-        var data = hxlProxyToJSON(response);
-        createAppealsTable(data);
-        }
-});*/
-
+// Get documents data
+// Not needed right now
 $.ajax({
     type: 'GET',
     url: hxlDocumentsCallURL,
     dataType: 'json',
     success:function(response){
         response = hxlProxyToJSON(response);
+        // Create document table if needed
         appeals.forEach(function(a){
             $('#documents').append('<h3 id="appealname'+a+'" class="documenttitle"></h3><table id="documenttable'+a+'"><tr><th>Document</th><th>Country</th><th>Date</th></tr></table>');
             response.forEach(function(d){
@@ -208,18 +205,7 @@ $.ajax({
         });
     }
 });
-
-//example key figures running off a spreadsheet.
-loadKeyFigures('https://docs.google.com/spreadsheets/d/1EFFsb87GPVrKqjaBUQCQb6PRbvUEPwoAEHo0BhkTZ28/edit?usp=sharing');
-/*
-Here is a few suggestions:
-Maps of countries with open operations.
-Overview of:
-DREF and Emergency Appeals launched in the four countries - x
-Funding level - x
-Links to relevant appeal documents - x
-Global tools deployed
-Operational teams in place
-Public documents (joint statement, press release)
-Links to appeal plus pages for each operation - x
 */
+
+// Key figures from Europe Regional Office spreadsheet
+loadKeyFigures('https://docs.google.com/spreadsheets/d/1EFFsb87GPVrKqjaBUQCQb6PRbvUEPwoAEHo0BhkTZ28/edit?usp=sharing');
